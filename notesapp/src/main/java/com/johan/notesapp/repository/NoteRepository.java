a925b3a04ca740cdf5a8229031dcf3347c30d49b
Johan Costa <159167858+johancosta999@users.noreply.github.com>
2026-05-24 13:50:54 +0530

    Revert "basic functions - backend implementation"
    This reverts commit 2d1090d8edae08b0229c2a411a9e7ba51fc8a2cf.

--- notesapp/src/main/java/com/johan/notesapp/repository/NoteRepository.java
+++
@@ -1,8 +0,0 @@
-package com.johan.notesapp.repository;
-
-import com.johan.notesapp.model.Note;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface NoteRepository extends JpaRepository<Note,Long> {}
-
